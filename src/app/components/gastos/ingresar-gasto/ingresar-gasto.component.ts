b762dbaa5c42609f4deaa5727c8e028d1177fe3f
br1lisboa <brlisbo@gmail.com>
2022-10-27 11:46:08 -0300

    finished warning errors in compgastos for incorrect inputs

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingresar-gasto.component.css']
 })
 export class IngresarGastoComponent implements OnInit {
+  nombreGasto: string;
+  cantidad: number;
+  formIncorrecto: boolean;
+  textIncorrect: string;
 
-  constructor() { }
+  constructor() {
+    this.nombreGasto = '';
+    this.cantidad = 0;
+    this.formIncorrecto = false;
+    this.textIncorrect = 'Nombre gasto o cantidad incorrecta'
+  };
 
   ngOnInit(): void {
-  }
+  };
 
-}
+  agregarGasto() {
+
+    if (this.nombreGasto === '' || this.cantidad <= 0) {
+      this.formIncorrecto = true
+    } else {
+      this.formIncorrecto = false
+      this.nombreGasto = ''
+      this.cantidad = 0
+    }
+
+  };
+
+};
