47652ab4aca6f6063654ff5534990db065e1911c
br1lisboa <brlisbo@gmail.com>
2022-10-27 12:15:21 -0300

    ondestroy up

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-listar-gasto',
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent implements OnInit {
+export class ListarGastoComponent implements OnInit, OnDestroy {
+  subcription: Subscription;
 
   constructor(private _presupuestoService: PresupuestoService) {
-    this._presupuestoService.getGastos().subscribe(data => {
+    this.subcription = this._presupuestoService.getGastos().subscribe(data => {
       console.log(data)
     });
   };
 
   ngOnInit(): void {
   };
+
+  ngOnDestroy(): void {
+    this.subcription.unsubscribe()
+  }
 
 };
