b7a838b37cf0364ee8ea70a0e42e1e62da2c2a57
br1lisboa <brlisbo@gmail.com>
2022-10-27 08:47:47 -0300

    added syles fonts | color | some routes default | bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/ingresarPresupuesto', pathMatch: 'full' },
+  { path: 'ingresarPresupuesto', component: IngresarPresupuestoComponent },
+  { path: 'gastos', component: GastosComponent },
+  { path: '**', redirectTo: '/ingresarPresupuesto', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
