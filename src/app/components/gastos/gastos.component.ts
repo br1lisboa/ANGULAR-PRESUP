d74989b704ccf5b26f1c994aca8cde8715da0c18
br1lisboa <brlisbo@gmail.com>
2022-10-27 11:21:08 -0300

    finished redirections when presup is 0

--- src/app/components/gastos/gastos.component.ts
+++ src/app/components/gastos/gastos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
@@ -10,11 +11,14 @@ export class GastosComponent implements OnInit {
   // Interpoklacion var publics y privadas
   // presupuesto = 0
 
-  constructor(private _presupuestoService: PresupuestoService) { }
+  constructor(private _presupuestoService: PresupuestoService, private _router: Router) { }
 
   ngOnInit(): void {
+    if (this._presupuestoService.presupuesto === 0) {
+      this._router.navigate(['/ingresarPresupuesto'])
+    };
     // this.presupuesto = this._presupuestoService.presupuesto; //> Solo a fines educativos o para yo aplicar
     console.log(this._presupuestoService.presupuesto)
-  }
+  };
 
-}
+};
